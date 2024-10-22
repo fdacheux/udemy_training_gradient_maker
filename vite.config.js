56c9e308f2faac3d6ba7163d2e14499a64ddaf5d
Fanny Dacheux <fanny.dacheux@gmail.com>
2024-10-22 12:57:52 +0200

    base url fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/gradient_maker/",
+  base: "/udemy_training_gradient_maker/",
 });
